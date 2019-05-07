67fbac8202aff5de89d8c341d968ce6ff9ff464d
Luque <michael.luque@qnexapp.com>
2019-05-06 23:42:35 -0500

    Agregado pagina contacto

--- js/agrovetmarket.js
+++ js/agrovetmarket.js
@@ -110,6 +110,26 @@ $(function () {
 
     });
 
+    /* Ripple effect
+    *****************************************************/
+    var $ripples = $('.ripples');
+    $ripples.on('click.Ripples', function (e) {
+        var $this = $(this);
+        var $offset = $this.parent().offset();
+        var $circle = $this.find('.ripplesCircle');
+        var x = e.pageX - $offset.left;
+        var y = e.pageY - $offset.top;
+        $circle.css({
+            top: y + 'px',
+            left: x + 'px'
+        });
+        $this.addClass('is-active');
+    });
+    $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function (e) {
+        $(this).removeClass('is-active');
+    });
+
+
 
 });
 
