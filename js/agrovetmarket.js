4d3207e618d065100e504f08f5b334145ee4b826
Luque <michael.luque@qnexapp.com>
2019-05-22 17:30:05 -0500

    animaciones fade - agregado pagina oficinas

--- js/agrovetmarket.js
+++ js/agrovetmarket.js
@@ -54,29 +54,31 @@ $(function () {
   /* Header Small / Tall  - ONLY DESKTOP
   *****************************************************/
   if (!isMobileDevice()) {
+
+
     $(window).scroll(function () {
       var $nav = $('.nav-principal');
-      if ($(window).scrollTop() > 100) {
-        $nav.addClass('small');
-        $nav.find('.logo').addClass('small');
-        $nav.find('.search-form').addClass('small');
-        $nav.find('.navbar-nav').removeClass('mt-5').css('margin-top', '-15px');
-        $('.nav-top').find('.search-form-top').addClass('small');
-        $('#carousel-principal.carousel').addClass('small');
+      if ($(window).scrollTop() > 30) {
+        $nav.addClass('small');        
+        /*$nav.find('.navbar-nav').removeClass('mt-5').css('margin-top', '-15px');*/
+        $('.nav-top').addClass('small');
+        /*$('#carousel-principal.carousel').addClass('small');*/
       } else {
         $nav.removeClass('small');
-        $nav.find('.logo').removeClass('small');
-        $nav.find('.search-form').removeClass('small');
-        $nav.find('.navbar-nav').addClass('mt-5');
+        $('.nav-top').removeClass('small').removeClass('no-items');
+
+        /*$nav.find('.navbar-nav').addClass('mt-5');*/
         $nav.find('.navbar-nav').removeAttr('style');
         $('#carousel-principal.carousel').removeClass('small');
-        $('.nav-top').find('.search-form-top').removeClass('small');
+        
         //reset search-top-nav
         $('.search-form-top').find('form').removeAttr('style');
         $('.search-form-top').find('img').attr('src', 'images/icons/icon-lupa-small.png');
         $('.search-form-top').find('.btn-nav-top-search').attr('data-action', 'open')
       }
     });
+
+
   } else {
     var $nav = $('.nav-principal');
     $nav.addClass('small');
@@ -92,6 +94,8 @@ $(function () {
   $('.btn-nav-top-search').on('click', function (e) {
     e.preventDefault();
     if ($(this).attr('data-action') == 'open') {
+      //hidden nav-items
+      $(this).closest('.nav-top').addClass('no-items')
       //change icon
       $(this).find('img').attr('src', 'images/icons/icon-lupa-small-close.png');
       //show input 
@@ -101,6 +105,8 @@ $(function () {
       //change action state button
       $(this).attr('data-action', 'close')
     } else {
+      //hidden nav-items
+      $(this).closest('.nav-top').removeClass('no-items')
       //change icon
       $(this).find('img').attr('src', 'images/icons/icon-lupa-small.png');
       //hide input 
