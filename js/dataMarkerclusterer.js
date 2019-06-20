93fdd710dc2ab8d064ed7f97459b036477864b33
Luque <michael.luque@qnexapp.com>
2019-06-20 03:50:07 -0500

    fix comillas en el JSON

--- js/dataMarkerclusterer.js
+++ js/dataMarkerclusterer.js
@@ -6,8 +6,8 @@ var dataMarker = {
             phone: '54 054 950 710 204',
             mail: 'agricotramg@gmail.com',
             contact: 'Alicia Choquehuanca',
-            lat: -16.412971, 
-            lng: -71.5485019
+            lat: "-16.412971", 
+            lng: "-71.5485019"
         },
         {
             name: 'AGROCOMERCIO S.R.L',
@@ -15,8 +15,8 @@ var dataMarker = {
             phone: '54 054 959 650 648',
             mail: 'agrocomercio_aqp@yahoo.com',
             contact: 'Esperanza Pinto',
-            lat: -16.409, 
-            lng: -71.5381588
+            lat: "-16.409", 
+            lng: "-71.5381588"
         },
         {
             name: 'BIOGENESIS PERÚ E.I.R.L.',
@@ -24,44 +24,8 @@ var dataMarker = {
             phone: '54 054 959 752586',
             mail: 'biogenesisperu@hotmail.com',
             contact: 'Sheyla Astrulla Santa Cruz',
-            lat: -16.400513, 
-            lng: -71.5377682
-        },
-        {
-            name: 'CASA DEL BUEN CRIADOR EIRL',
-            address: 'Av. Parra 367 ',
-            phone: '54 054 959 866359',
-            mail: 'lacasadelbuencriador@yahoo.es',
-            contact: 'Lourdes Villegas Flores ',
-            lat: -16.414063, 
-            lng: -71.5490127
-        },
-        {
-            name: 'Universo Veterinario S.A.C',
-            address: 'Majes,MZA. Q LOTE. 4 EL PEDREGAL',
-            phone: '51 54 586 867',
-            mail: 'universoveterinario1@hotmail.com',
-            contact: 'María Morales',
-            lat: -16.356546, 
-            lng: -72.190614
-        },
-        {
-            name: 'Veterinaria Lucas',
-            address: 'Av. Parra 390 - Cercado',
-            phone: '54 054 226 044',
-            mail: 'veterinaria_lucas@hotmail.com',
-            contact: 'Victor Martinez',
-            lat: -16.4153633, 
-            lng: -71.5498878
-        },
-        {
-            name: 'Veterinaria Sanidad Pecuaria',
-            address: 'Av. Majes Mz. 3E Lote A12 El pedregal',
-            phone: '54 054 586 420',
-            mail: 'sanidadpecuaria@hotmail.com',
-            contact: 'Amanda Lescano Valencia',
-            lat: -16.358692, 
-            lng: -72.190452
+            lat: "-16.400513", 
+            lng: "-71.5377682"
         }
     ]    
 }
